71691dd83b7e48c4a3985982514a63130baef70d
duong <linhduong2771999@gmail.com>
2020-03-12 14:45:18 +0700

    test_authenticate

--- src/modules/Login/Login.js
+++ src/modules/Login/Login.js
@@ -1,8 +1,51 @@
 import React, { Component } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Input, Checkbox, Button } from "antd";
+import Firebase from "../../config/FirebaseClient";
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+  onChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  onSubmit = (event) => {
+    const { email, password } = this.state;
+    event.preventDefault();
+    Firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(error => {
+        var errorCode = error.code;
+        // var errorMessage = error.message;
+        if (errorCode === "auth/wrong-password") {
+          alert("Wrong password.");
+        } else {
+          alert("Sai mật khẩu hoặc tài khoản !!!");
+        }
+        console.log(error);
+      });
+  };  
+  logout = () => {
+    Firebase.auth().signOut();
+  }
+
   render() {
+    Firebase.auth().onAuthStateChanged((user) =>{
+      if(user){
+        console.log("Login successfully.")
+      }else{
+        console.log("Failed.");
+      }
+    })
     return (
       <div className="login-wrapper">
         <div className="container h-100">
@@ -14,20 +57,31 @@ class Login extends Component {
                     <h4>Login your account</h4>
                   </div>
                   <div className="login-form mt-4">
-                    <form>
+                    <form onSubmit={this.onSubmit}>
                       <div className="form-group">
-                        <Input placeholder="Email" />
+                        <Input
+                          placeholder="Email"
+                          name="email"
+                          onChange={this.onChange}
+                        />
                       </div>
                       <div className="form-group">
-                        <Input.Password placeholder="Password" />
+                        <Input.Password
+                          placeholder="Password"
+                          name="password"
+                          onChange={this.onChange}
+                        />
                       </div>
                       <div className="checkbox">
                         <Checkbox>Remember me</Checkbox>
                       </div>
                       <div className="form-group mt-3">
-                        <Button type="primary" block>
+                        <Button htmlType="submit" type="primary" block>
                           Login
                         </Button>
+                        <button type="button" onClick={this.logout} >
+                          Logout
+                        </button>
                       </div>
                     </form>
                   </div>
