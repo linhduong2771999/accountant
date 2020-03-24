508581d66944df5f0d021c4b1e63e6917f333088
duong <linhduong2771999@gmail.com>
2020-03-24 23:39:38 +0700

    create_signup

--- src/modules/Login/Login.js
+++ src/modules/Login/Login.js
@@ -18,7 +18,8 @@ class Login extends Component {
       user: "",
       isLoading: false,
       disabledButton: false,
-      textWarningPassword: false
+      textWarningPassword: false,
+      isActiveClass: true
     };
   }
 
@@ -69,10 +70,33 @@ class Login extends Component {
           alert("Sai mật khẩu hoặc tài khoản !!!");
         } else if (errorCode === "auth/network-request-failed") {
           Notifies.errorMessege();
-        } else{
+        } else {
           alert("Tài khoản không tồn tại !!!");
         }
         // console.log(error);
+      });
+  };
+
+  userSignup = event => {
+    const { email, password } = this.state;
+    event.preventDefault();
+
+    Firebase.auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(error => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode === "auth/weak-password") {
+          alert("Mật khẩu quá yếu");
+        } else if(errorCode === "auth/email-already-in-use") {
+          alert("Tài khoản đã tồn tại");
+        } else if(errorCode === "auth/invalid-email") {
+          alert("Email không hợp lệ")
+        }
+        else {
+          alert(errorMessage);
+        }
+        console.log(error);
       });
   };
 
@@ -83,6 +107,7 @@ class Login extends Component {
           if (user) {
             // this.props.actions.loginAccountSuccess(true);
             if (this._isMounted) {
+              console.log("singup")
               this.setState({
                 user
               });
@@ -116,8 +141,18 @@ class Login extends Component {
     this.onAuthStateChanged();
   };
 
+  isActiveLogin = () => {
+    this.setState({
+      isActiveClass: true
+    });
+  };
+  isActiveSignup = () => {
+    this.setState({
+      isActiveClass: false
+    });
+  };
   render() {
-    const { disabledButton, textWarningPassword } = this.state;
+    const { disabledButton, textWarningPassword, isActiveClass } = this.state;
     // var user = Firebase.auth().currentUser;
     if (this.state.user) return <Redirect to="/" />;
     return (
@@ -127,55 +162,144 @@ class Login extends Component {
           <div className="row h-100 justify-content-center align-items-center">
             <div className="col-xl-4 col-lg-4 col-md-7 col-sm-12 col-12">
               <div className="card">
-                <div className="card-body p-4">
-                  <div className="card-title mt-2 mb-5">
-                    <h4>Đăng nhập tài khoản</h4>
-                  </div>
-                  <div className="login-form mt-4">
-                    <form onSubmit={this.userLogin}>
-                      <div className="form-group">
-                        <Input
-                          placeholder="Email"
-                          name="email"
-                          onChange={this.onChange}
-                          autoComplete="on"
-                        />
-                      </div>
-                      <div className="form-group">
-                        <Input.Password
-                          placeholder="mật khẩu"
-                          name="password"
-                          onChange={this.onChange}
-                        />
-                        {textWarningPassword ? (
-                          <span
-                            style={{ fontSize: "0.75em" }}
-                            className="text-danger"
+                <ul className="nav nav-tabs nav-tabs-custom">
+                  <li
+                    style={
+                      isActiveClass
+                        ? { backgroundColor: "rgba(196, 185, 185, 0.747)" }
+                        : null
+                    }
+                    className="nav-link-custom col-6 text-center text-uppercase"
+                  >
+                    <a
+                      data-toggle="tab"
+                      href="#login"
+                      onClick={this.isActiveLogin}
+                    >
+                      Đăng nhập
+                    </a>
+                  </li>
+                  <li
+                    style={
+                      isActiveClass
+                        ? null
+                        : { backgroundColor: "rgba(196, 185, 185, 0.747)" }
+                    }
+                    className="nav-link-custom col-6 text-center text-uppercase "
+                  >
+                    <a
+                      data-toggle="tab"
+                      href="#signup"
+                      onClick={this.isActiveSignup}
+                    >
+                      Đăng Ký
+                    </a>
+                  </li>
+                </ul>
+                <div className="tab-content">
+                  <div
+                    className="tab-pane fade show active card-body px-4 pb-4 text-warning"
+                    id="login"
+                    aria-labelledby="s"
+                  >
+                    <div className="card-title mb-4">
+                      <h4>Đăng nhập tài khoản</h4>
+                    </div>
+                    <div className="login-form mt-4">
+                      <form onSubmit={this.userLogin}>
+                        <div className="form-group">
+                          <Input
+                            placeholder="Email"
+                            name="email"
+                            onChange={this.onChange}
+                            autoComplete="on"
+                          />
+                        </div>
+                        <div className="form-group">
+                          <Input.Password
+                            placeholder="mật khẩu"
+                            name="password"
+                            onChange={this.onChange}
+                          />
+                          {textWarningPassword ? (
+                            <span
+                              style={{ fontSize: "0.75em" }}
+                              className="text-danger"
+                            >
+                              ( Mật khẩu phải có ít nhất 1 chữ in hoa, số và 8
+                              ký tự )
+                            </span>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                        <div className="checkbox">
+                          <Checkbox>Ghi nhớ</Checkbox>
+                        </div>
+                        <div className="form-group mt-3">
+                          <Button
+                            disabled={disabledButton ? false : true}
+                            htmlType="submit"
+                            type="primary"
+                            block
                           >
-                            ( Mật khẩu phải có ít nhất 1 chữ in hoa, số và 8 ký tự )
-                          </span>
-                        ) : (
-                          ""
-                        )}
-                      </div>
-                      <div className="checkbox">
-                        <Checkbox>Ghi nhớ</Checkbox>
-                      </div>
-                      <div className="form-group mt-3">
-                        <Button
-                          disabled={disabledButton ? false : true}
-                          htmlType="submit"
-                          type="primary"
-                          block
-                        >
-                          Đăng nhập
-                        </Button>
-                      </div>
-                    </form>
+                            Đăng nhập
+                          </Button>
+                        </div>
+                      </form>
+                    </div>
+                    <div className="text-center">
+                      <i className="fa fa-lock"></i>
+                      <Link to="/reset">Quên mật khẩu ?</Link>
+                    </div>
                   </div>
-                  <div className="text-center">
-                    <i className="fa fa-lock"></i>
-                    <Link to="/reset">Quên mật khẩu ?</Link>
+                  <div
+                    id="signup"
+                    className="tab-pane card-body px-4 pb-4 text-warning"
+                  >
+                    <div className="card-title mb-4">
+                      <h4>Đăng ký tài khoản</h4>
+                    </div>
+                    <div className="login-form mt-4">
+                      <form onSubmit={this.userSignup}>
+                        <div className="form-group">
+                          <Input
+                            placeholder="Email"
+                            name="email"
+                            onChange={this.onChange}
+                            autoComplete="on"
+                          />
+                        </div>
+                        <div className="form-group">
+                          <Input.Password
+                            placeholder="mật khẩu"
+                            name="password"
+                            onChange={this.onChange}
+                          />
+                          {textWarningPassword ? (
+                            <span
+                              style={{ fontSize: "0.75em" }}
+                              className="text-danger"
+                            >
+                              ( Mật khẩu phải có ít nhất 1 chữ in hoa, số và 8
+                              ký tự )
+                            </span>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                        <div className="form-group mt-3">
+                          <Button
+                            disabled={disabledButton ? false : true}
+                            htmlType="submit"
+                            type="primary"
+                            block
+                          >
+                            Đăng Ký
+                          </Button>
+                        </div>
+                      </form>
+                    </div>
                   </div>
                 </div>
               </div>
